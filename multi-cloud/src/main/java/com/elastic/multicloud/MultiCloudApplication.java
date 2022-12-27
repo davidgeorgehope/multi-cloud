b99fcbf7865b745be98ff35fc9fd2d163bed99be
David Hope <davidhope@davids-mbp.lan>
2022-12-27 11:13:31 -0500

    first code

---
+++ multi-cloud/src/main/java/com/elastic/multicloud/MultiCloudApplication.java
@@ -0,0 +1,13 @@
+package com.elastic.multicloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MultiCloudApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MultiCloudApplication.class, args);
+	}
+
+}
